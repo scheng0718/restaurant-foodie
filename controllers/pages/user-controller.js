a22083719bd6577dd314c6a77a069c1f40d077fe
scheng0718 <scheng0718@gmail.com>
2023-09-13 14:40:46 -0700

    feat: add user-services and signup api

--- controllers/pages/user-controller.js
+++ controllers/pages/user-controller.js
@@ -1,32 +1,19 @@
-const bcrypt = require('bcryptjs')
+// const bcrypt = require('bcryptjs')
 const { User, Comment, Restaurant, Favorite, Like, Followship } = require('../../models')
 const { imgurFileHandler } = require('../../helpers/file-helpers')
+const userServices = require('../../services/user-services')
 
 const userController = {
   signUpPage: (req, res) => {
     res.render('signup')
   },
   signUp: (req, res, next) => {
-    if (req.body.password !== req.body.passwordCheck) {
-      throw new Error('Passwords do not match')
-    }
-    User.findOne({ where: { email: req.body.email } })
-      .then(user => {
-        if (user) {
-          throw new Error('Email already exists')
-        }
-        return bcrypt.hash(req.body.password, 10)
-      })
-      .then(hash => User.create({
-        name: req.body.name,
-        email: req.body.email,
-        password: hash
-      }))
-      .then(() => {
-        req.flash('success_messages', 'Your account is created successfully')
-        res.redirect('/signin')
-      })
-      .catch(err => next(err))
+    userServices.signUp(req, (err, data) => {
+      if (err) return next(err)
+      req.flash('success_messages', 'Your account is created successfully')
+      req.session.createdData = data
+      return res.redirect('/signin')
+    })
   },
   signInPage: (req, res) => {
     res.render('signin')
