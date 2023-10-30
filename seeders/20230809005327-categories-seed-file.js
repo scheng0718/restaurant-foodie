78294982cdf6acbcb7e04f5f46af288e1f7cbdcd
scheng0718 <scheng0718@gmail.com>
2023-10-30 00:21:17 -0700

    feat: modify categories language

--- seeders/20230809005327-categories-seed-file.js
+++ seeders/20230809005327-categories-seed-file.js
@@ -2,7 +2,7 @@
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.bulkInsert('Categories',
-      ['中式料理', '日本料理', '義大利料理', '墨西哥料理', '素食料理', '美式料理', '複合式料理']
+      ['Chinese Cuisine', 'Japanese Cuisine', 'Italian Cuisine', 'Mexican Cuisine', 'Vegetarian Cuisine', 'American Cuisine', 'Fusion Cuisine']
         .map(item => {
           return {
             name: item,
